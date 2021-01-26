55db2a2fafd41aee6c3cda62ce414d89b4db8eed
Arif jamal <arifjamal2012@gmail.com>
2021-01-26 23:31:23 +0530

    done

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ function fetchdata() {
 			return res.json();
 		})
 		.then((data) => {
-			const html = data.data.map((user) => {
-				return `
+			const html = data.data
+				.map((user) => {
+					return `
       <div class='user'>
-      <h3>Name: ${user.businessName}</h3>
-      <h4>Business Start Date: ${user.businessStartDate}</h4>
-      <p>Status: ${user.status}</p>
-      <h4>Price Plan: ${user.pricePlan}</h4>
-      <h4>Phone No: ${user.blingPhoneNumber}</h4>
-      <p>Manager Name: ${user.managerName}</p>
-      <h4>Business Id: ${user.businessId}</h4>
+        <h3>Name: ${user.businessName}</h3>
+        <h4>Business Start Date: ${user.businessStartDate}</h4>
+        <p>Status: ${user.status}</p>
+        <h4>Price Plan: ${user.pricePlan}</h4>
+        <h4>Phone No: ${user.blingPhoneNumber}</h4>
+        <p>Manager Name: ${user.managerName}</p>
+        <h4>Business Id: ${user.businessId}</h4>
       </div>
       `;
-			});
+				})
+				.join('');
 
 			document.querySelector('#app').insertAdjacentHTML('afterbegin', html);
 		})
