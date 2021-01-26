f7fab5e0feaee2c65ef8fcdbe753defa6efbcecd
Arif jamal <arifjamal2012@gmail.com>
2021-01-26 21:39:55 +0530

    tasks

---
+++ app.js
@@ -0,0 +1,22 @@
+fetch('demo.json')
+	.then((res) => {
+		//console.log(res);
+		return res.json();
+	})
+	.then((data) => {
+		const html = data.data.map((user) => {
+			return `
+      <div class='user'>
+      <h3>Name: ${user.businessName}</h3>
+      <h4>Business Start Date: ${user.businessStartDate}</h4>
+      <p>Status: ${user.status}</p>
+      <h4>Price Plan: ${user.pricePlan}</h4>
+      <h4>Phone No: ${user.blingPhoneNumber}</h4>
+      <p>Manager Name: ${user.managerName}</p>
+      <h4>Business Id: ${user.businessId}</h4>
+      </div>
+      `;
+		});
+
+		document.querySelector('#app').insertAdjacentHTML('afterbegin', html);
+	});
